df177a4b56d34bb5890cdbcc456968b5404da197
josemarialanda <josemaria.landa@gmail.com>
2023-03-09 18:07:47 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "NixOS system configurations";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+	    url = "github:nix-community/home-manager";
+	    inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: 
+  let system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; };
+      };
+      lib = nixpkgs.lib;
+  in {
+    homeManagerConfigurations = {
+      josemaria = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./user/home.nix ];
+      };
+    };
+    nixosConfigurations = {
+      nixos = lib.nixosSystem { 
+        inherit system; 
+        modules = [ ./system/configuration.nix ]; 
+      };
+    };
+  };
+}
