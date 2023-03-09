117ff37522ee805459919c8befbd5a43b0d744dc
josemarialanda <josemaria.landa@gmail.com>
2023-03-09 20:13:29 +0000

    ${1}

--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@ if test -z '$1'; then
   echo "You forgot to enter a commit message."
 else
   git add --all
-  git commit -m '$1'
+  git commit -m '${1}'
   git push --set-upstream origin master
 fi
