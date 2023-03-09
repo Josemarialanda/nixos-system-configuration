34c1749cc15bd370816001ca6039b2787caaca87
josemarialanda <josemaria.landa@gmail.com>
2023-03-09 20:18:15 +0000

    jue 09 mar 2023 20:18:15 GMT: fix push script commit message formatting

--- push.sh
+++ push.sh
@@ -5,6 +5,6 @@ if test -z '$1'; then
   echo "You forgot to enter a commit message."
 else
   git add --all
-  git commit -m "$COMMIT_TITLE:$COMMIT_MSG"
+  git commit -m "$COMMIT_TITLE: $COMMIT_MSG"
   git push --set-upstream origin master
 fi
