4f94770c38addcfb8482f84c65c137eefeff0d61
josemarialanda <josemaria.landa@gmail.com>
2023-03-09 20:15:59 +0000

    jue 09 mar 2023 20:15:59 GMT

--- push.sh
+++ push.sh
@@ -1,7 +1,9 @@
+COMMIT_TITLE=$(date)
+
 if test -z '$1'; then
   echo "You forgot to enter a commit message."
 else
   git add --all
-  git commit -m '${1}'
+  git commit -m "$COMMIT_TITLE"
   git push --set-upstream origin master
 fi
