dac499dcb819d04836ffff940f6420c7e5e3335f
josemarialanda <josemaria.landa@gmail.com>
2023-03-10 14:04:42 +0000

    vie 10 mar 2023 14:04:42 GMT: remove remote script

--- apply-remote.sh
+++
@@ -1,9 +0,0 @@
-wget https://github.com/Josemarialanda/nixos-system-configuration/archive/master.tar.gz
-tar -xf master.tar.gz
-pushd nixos-system-configuration-master
-./update-system.sh "boot"
-./update-home.sh
-popd
-rm -rf nixos-system-configuration-master
-rm -f master.tar.gz
-echo "Reboot computer for changes to take effect"
--- apply-system.sh
+++ apply-system.sh
@@ -1,11 +1,11 @@
 flag=$1
 
 if [ $flag = "boot" ]; then
-  nixos-rebuild boot --impure --flake .#
+  sudo nixos-rebuild boot --impure --flake .#
 elif [ $flag = "switch" ]; then
-  nixos-rebuild switch --impure --flake .#
+  sudo nixos-rebuild switch --impure --flake .#
 elif [ $flag = "build" ]; then
-  nixos-rebuild build --impure --flake .#
+  sudo nixos-rebuild build --impure --flake .#
 else
   echo "Options are boot, switch or build."
 fi
