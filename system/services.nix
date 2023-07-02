93247e054b618e390406cf4f5ac1434413ddd803
josemarialanda <josemaria.landa@gmail.com>
2023-07-03 00:19:45 +0100

    disable system sleep if there is an active SSH connection

--- system/services.nix
+++ system/services.nix
@@ -2,8 +2,43 @@
 
 let 
   config-variables = import ../config-variables.nix;
+  # create script to prevent system sleep if there is an active SSH connection
+  noSleepOnSSH = pkgs.writeScriptBin "noSleepOnSSH" ''
+    #!/bin/sh
+    sshConnected=$(${pkgs.unixtools.netstat}/bin/netstat -tnp -W 2>/dev/null | ${pkgs.gawk}/bin/gawk '$6 == "ESTABLISHED" && $4 ~ /:22$/ {print "true"; found = 1; exit} END { if (found != 1) print "false" }')
+    echo $sshConnected
+    if [ "$sshConnected" = "true" ]; then
+      # Jiggle the mouse to prevent sleep
+      export DISPLAY=:0
+      export XAUTHORITY=/run/user/1000/.mutter-Xwaylandauth.IKCS71
+      ${pkgs.xdotool}/bin/xdotool mousemove_relative --sync 1 1
+      sleep 0.1
+      ${pkgs.xdotool}/bin/xdotool mousemove_relative --sync -- -1 -1
+      sleep 0.1
+      # write to journalctl (include timestamp)
+      ${pkgs.util-linux}/bin/logger -t noSleepOnSSH "Active SSH connection, jiggled mouse to prevent sleep"
+    else 
+      # write to journalctl (include timestamp)
+      ${pkgs.util-linux}/bin/logger -t noSleepOnSSH "No active SSH connection"
+    fi
+  '';
 in {
   config = {
+    # SystemD services configuration.
+    systemd.services = {
+      # Disable system sleep if there is an active SSH connection.
+      noSleepOnSSH = {
+        description = "Prevent system sleep if there is an active SSH connection";
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          Type = "simple";
+          Restart = "always";
+          RestartSec = "300";
+          ExecStart = "${noSleepOnSSH}/bin/noSleepOnSSH";
+        };
+      };
+    };
+
     services = {
       # Enable the OpenSSH daemon.
       openssh.enable = true;
